299c0182f63a07484f26bc7c3e5dd65ac6a4b61f
LiYuan1229 <823260355@qq.com>
2020-09-23 22:37:23 +0800

    [liyuan]:finish exercise-1

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,28 +1,17 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter, Link, Route,Switch } from "react-router-dom";
+import {BrowserRouter, Route,Switch } from "react-router-dom";
 import Home from "./Home";
 import MyProfile from "./MyProfile";
 import AboutUs from "./AboutUs";
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-        <div style= {{background: 'grey',height:'50px'}}>
-        <ul className="nav justify-content-end" >
-            <li >
-              <Link  to="/" style= {{color:'black',fontSize:'30px'}}>Home</Link>
-            </li>
-            <li >
-              <Link  to="/my-profile" style= {{color:'black',fontSize:'30px'}}>My Profile</Link>
-            </li>
-            <li >
-              <Link to="/about-us" style= {{color:'black',fontSize:'30px'}}>About Us</Link>
-            </li>
-          </ul>
-         </div>  
+        <Header /> 
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/my-profile" component={MyProfile} />
