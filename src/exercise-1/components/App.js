6ac858477dfed807d8f32ecaa239480f4efe98a9
LiYuan1229 <823260355@qq.com>
2020-09-23 22:26:04 +0800

    [liyuan]:finish exercise-1

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter, Link, Route,Switch } from "react-router-dom";
+import Home from "./Home";
+import MyProfile from "./MyProfile";
+import AboutUs from "./AboutUs";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router />
+        <BrowserRouter>
+        <div style= {{background: 'grey',height:'50px'}}>
+        <ul className="nav justify-content-end" >
+            <li >
+              <Link  to="/" style= {{color:'black',fontSize:'30px'}}>Home</Link>
+            </li>
+            <li >
+              <Link  to="/my-profile" style= {{color:'black',fontSize:'30px'}}>My Profile</Link>
+            </li>
+            <li >
+              <Link to="/about-us" style= {{color:'black',fontSize:'30px'}}>About Us</Link>
+            </li>
+          </ul>
+         </div>  
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/my-profile" component={MyProfile} />
+            <Route exact path="/about-us" component={AboutUs} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './exercise-1/components/App';
 import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
